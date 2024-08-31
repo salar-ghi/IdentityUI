03d7f0425e826c9223b6a26d229b5a1cd5431811
s.ghahremani <salar.ghi1993@gmail.com>
2024-08-31 17:00:07 +0330

    mino change

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect , useState } from 'react'
 import { useRouter } from 'next/navigation';
 import base from '@/app/api/base';
+import { useSearchParams } from 'next/navigation';
+import { headers } from 'next/headers';
 
 const Signin: React.FC = () => {
     const [nationalId, setNationalId] = useState('');
@@ -9,23 +11,43 @@ const Signin: React.FC = () => {
     const [message, setMessage] = useState('');
     const [isSuccess, setIsSuccess] = useState(false);
     const router = useRouter();
+    const searchParams = useSearchParams();
+    const returnUrl = searchParams.get('returnUrl');
+
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
-            const response = await base.post('/Auth/login', {
-                nationalId, password
-            });
-            localStorage.setItem('token', response.data.token);
-            console.log('response.data.token ===>', response.data.token)
-            setMessage('Login successful!');
-            setIsSuccess(true);
-            router.push('/');
+            const isLoginSuccessful = await handleLogin(nationalId, password);
+            if (isLoginSuccessful)
+            {
+                console.log('if statement');
+                router.push('https://localhost:5000/apione/Message')
+            } else {
+                alert('Login failed. Please try again.');
+            }
         } catch (error) {
             setMessage(`Error: ${error.response?.data?.message || error.message}`);
             setIsSuccess(false);
         }
     };
+
+    const handleLogin = async (nationalId: string, password: string) => {
+        const response = await base.post('/Auth/login', {
+             nationalId, password
+        });
+        if ( response.status >= 200 && response.status < 300 ) {
+            console.log(response.status);
+            const data = await response.data.token;
+            localStorage.removeItem('token')
+            localStorage.setItem('token', data);
+            setIsSuccess(true);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     return (
         <div className="flex items-center justify-center max-h-screen">
             <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-lg w-96">
@@ -40,20 +62,18 @@ const Signin: React.FC = () => {
                 </div>
                 <div className="mb-4">
                     <input type="password" name="password" placeholder="Password"
-                        value={password} onChange={(e) => setPassword(e.target.value)} 
+                        value={password} onChange={(e) => setPassword(e.target.value)}
                         className="border p-2 w-full rounded"/>
                 </div>
                 <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">
                     Login
                 </button>
                 {message && (
-                    <p className={`mt-4 ${isSuccess ? 'text-green-500' : 'text-red-500'}`}>
-                        {message}
-                    </p>
+                    <p className={`mt-4 ${isSuccess ? 'text-green-500' : 'text-red-500'}`}> {message} </p>
                 )}
             </form>
         </div>
-    );    
+    );
 };
 
 export default Signin
